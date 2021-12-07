1e12a5a0f1d9f0fbd72d58f1f59f9185ac9fb18f
Lukas Prokop <admin@lukas-prokop.at>
2021-12-07 01:02:50 +0100

    Add slides and sources from last week's presentation.

---
+++ compile.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# teardown
+#rm -r build
+
+# setup
+mkdir -p build
+cp -r src/* build
+
+# build
+cd build
+lualatex -halt-on-error -shell-escape --enable-write18 slides.tex
+cd ..
+
+# finalize
+mv build/slides.pdf slides.pdf
